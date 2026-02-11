13faf9bbb223d310bb8d853c892db0b7f974615e
withcenterdev5 <withcenter.dev5@gmail.com>
2026-02-11 11:03:25 +0800

    refactor: improve code formatting and readability in my_provider.dart

--- lib/my_provider.dart
+++ lib/my_provider.dart
@@ -1,41 +1,70 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-
 // Create the counter model with initial state
-class CounterModel extends ChangeNotifier {
+class CounterModel
+    extends
+        ChangeNotifier {
+  // Initial state for count value
   int _count = 4;
 
+  // Increment method
   void increment() {
     _count++;
     notifyListeners();
   }
 }
 
-class CounterWidget extends StatelessWidget {
-  const CounterWidget({super.key,});
+class CounterWidget
+    extends
+        StatelessWidget {
+  const CounterWidget({
+    super.key,
+  });
 
   @override
-  build(context) {
+  build(
+    context,
+  ) {
     return Column(
       children: [
-        Consumer<CounterModel>(
-          builder: (_, model, _) => Text("Counter: ${model._count}"),
+        // Rebuild the text widget to display the updated state
+        Consumer<
+          CounterModel
+        >(
+          builder:
+              (
+                _,
+                model,
+                _,
+              ) => Text(
+                "Counter: ${model._count}",
+              ),
         ),
+        //Button for incrementing the count value from the count model
         ElevatedButton(
-          onPressed: () => context.read<CounterModel>().increment(),
-          child: const Text("Increment"),
+          onPressed: () => context
+              .read<
+                CounterModel
+              >()
+              .increment(),
+          child: const Text(
+            "Increment",
+          ),
         ),
       ],
     );
   }
 }
 
-
-void main() {
+void
+main() {
   runApp(
     ChangeNotifierProvider(
-      create:(_,) => CounterModel(),
+      create:
+          (
+            _,
+          ) => CounterModel(),
       child: MaterialApp(
         home: CounterWidget(),
       ),
